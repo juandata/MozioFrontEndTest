6f2b67ab384f510df9c3627a22b079d55f99acb8
Juan David Tabares Arce <juandavidtabaresarce@gmail.com>
2018-01-15 17:59:56 -0500

    Update Solutions.js

--- Solutions.js
+++ Solutions.js
@@ -7,3 +7,12 @@ String.prototype.repeatify = function(n) {
 };
 
 "Mozio".repeatify(3);
+
+/*Question 2: Return the file extension or "false" if no extension. Please explain how would you implement a unit test for this method. What would you test?
+*/
+function getFileExtension(file){
+  var res = file.indexOf(".") === -1 ?  false :  file.split(".").pop();
+  return res;
+}
+
+console.log(getFileExtension("mozio.png"));
