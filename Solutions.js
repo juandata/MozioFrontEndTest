019984295d00345c1529b8470553ff8ced546be6
Juan David Tabares Arce <juandavidtabaresarce@gmail.com>
2018-01-15 16:19:35 -0500

    Update Solutions.js

--- Solutions.js
+++ Solutions.js
@@ -1,4 +1,9 @@
 
 /*Question 1: Write a function that repeats the String with the following output: */
 
+String.prototype.repeatify = function(n) {
+  console.log(this.repeat(n));
+  return this.repeat(n);
+};
 
+"Mozio".repeatify(3);
