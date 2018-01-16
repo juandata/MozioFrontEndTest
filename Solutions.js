023fd39a93e305e31dd63b98661ae64186987727
Juan David Tabares Arce <juandavidtabaresarce@gmail.com>
2018-01-15 19:41:12 -0500

    Update Solutions.js

--- Solutions.js
+++ Solutions.js
@@ -16,3 +16,17 @@ function getFileExtension(file){
 }
 
 console.log(getFileExtension("mozio.png"));
+/*Unit Test: I test whether method("mozio.png")  equals  "png", if this is not the case then I return "The Method does not 
+return the extension", you can change the two  dots (".") in line 14 to space or something different to test the case. 
+**I use Jest for testing my React apps. 
+*/
+var unitTest = {};
+unitTest.checkInput = function(method){
+  if(method("mozio.png") === "png"){
+    return "Method working as expected";
+    }
+    else {
+      return "The Method does not return the extension";
+    }
+  };
+console.log(unitTest.checkInput(getFileExtension));
